4e3de3c317f776929b8d75b74a813775db0f3091
Abdel <Abdel@merca.team>
2023-12-22 18:51:59 +0100

    feat(crud): add livreur

--- frontend/client/src/components/LivreurTable.vue
+++ frontend/client/src/components/LivreurTable.vue
@@ -1,7 +1,13 @@
 <template>
   <v-container>
     <div style="margin: 10px">
-      <v-text-field v-model="search" :placeholder="'Nom du Livreur'" clearable dense rounded></v-text-field>
+      <v-text-field
+        v-model="search"
+        :placeholder="'Nom du Livreur'"
+        clearable
+        dense
+        rounded
+      ></v-text-field>
     </div>
     <v-data-table
       :headers="headers"
@@ -11,6 +17,83 @@
       :loading="loading"
       class="elevation-1"
     >
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>Livreurs</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-dialog v-model="dialog" max-width="500px">
+            <template v-slot:activator="{ props }">
+              <v-btn color="primary" dark class="mb-2" v-bind="props">
+                Ajouter un livreur
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="text-h5">{{ formTitle }}</span>
+              </v-card-title>
+
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12" sm="6" md="4">
+                      <FormKit
+                        validation="required"
+                        validation-visibility="live"
+                        type="text"
+                        name="name"
+                        id="name"
+                        label="Name*"
+                        help="Your full name"
+                        placeholder="“Jon Doe”"
+                        v-model="editedItem.name"
+                      />
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <FormKit
+                        type="date"
+                        label="Date de créaction"
+                        help="Entrer la date de création"
+                        v-model="creationDate"
+                      />
+                    </v-col>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                      style="
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                      "
+                    >
+                      <input
+                        label="Disponible"
+                        type="checkbox"
+                        v-model="editedItem.disponibility"
+                      />
+                      <strong style="margin-left: 0.5em">Disponible</strong>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue-darken-1" variant="text" @click="close">
+                  Cancel
+                </v-btn>
+                <v-btn color="blue-darken-1" variant="text" @click="save">
+                  Save
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      <template v-slot:[`item.creationDate`]="{ item }">
+        {{ new Date(item.creationDate).toLocaleDateString() }}
+      </template>
       <template v-slot:[`item.disponibility`]="{ item }">
         <v-icon size="small" color="success" v-if="item.disponibility">
           mdi-checkbox-marked-circle
@@ -48,50 +131,6 @@
     </v-dialog>
     <!-- Add this inside your <template> tag in LivreursTable.vue -->
 
-    <!-- Edit Dialog -->
-    <v-dialog v-model="editDialog" max-width="500px">
-      <v-card>
-        <v-card-title> Edit Livreur </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-form ref="editForm">
-              <v-text-field
-                v-model="editedItem.name"
-                label="Name"
-                required
-              ></v-text-field>
-              <v-switch
-                v-model="editedItem.disponibility"
-                label="disponibility"
-              ></v-switch>
-              <template v-slot:activator="{ on }">
-                <v-text-field
-                  v-model="date"
-                  label="Picker in menu"
-                  prepend-icon="event"
-                  readonly
-                  v-on="on"
-                ></v-text-field>
-              </template>
-              <v-date-picker
-                :max="new Date().toISOString().substr(0, 10)"
-                min="1950-01-01"
-                @change="save"
-              >
-              </v-date-picker>
-            </v-form>
-          </v-container>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text @click="closeEditDialog"
-            >Cancel</v-btn
-          >
-          <v-btn color="blue darken-1" text @click="saveEdit">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-
     <!-- Consult Dialog -->
     <v-dialog v-model="consultDialog" max-width="500px">
       <v-card>
@@ -125,11 +164,11 @@ export default {
   data() {
     return {
       headers: [
-        { title: "ID",align: 'start', key: "id" },
-        { title: "Name", align: 'center',key: "name" },
-        { title: "Creation Date",align: 'center', key: "creationDate" },
-        { title: "disponibilité",align: 'center', key: "disponibility" },
-        { title: "Actions",align: 'end', key: "actions", sortable: false },
+        { title: "ID", align: "start", key: "id" },
+        { title: "Name", align: "center", key: "name" },
+        { title: "Creation Date", align: "center", key: "creationDate" },
+        { title: "disponibilité", align: "center", key: "disponibility" },
+        { title: "Actions", align: "end", key: "actions", sortable: false },
       ],
       livreurs: [],
       options: {},
@@ -138,11 +177,17 @@ export default {
       deleteDialog: false,
       selectedLivreur: null,
       snackbar: { show: false, color: "", message: "" },
-      editDialog: false,
       consultDialog: false,
-      editedItem: {}, // This will hold the item being edited
+      editedItem: {
+        id: "",
+        name: "",
+        creationDate: new Date(),
+        disponibility: false,
+      }, // This will hold the item being edited
       consultedItem: {}, // This will hold the item being consulted
       search: "",
+      editedIndex: -1,
+      dialog: false,
     };
   },
   watch: {
@@ -211,34 +256,79 @@ export default {
     },
     openEditDialog(item) {
       this.editedItem = Object.assign({}, item); // Create a copy of the item
-      this.editDialog = true;
+      this.dialog = true;
     },
-
-    closeEditDialog() {
-      this.editDialog = false;
-    },
-
     saveEdit() {
       LivreurService.updateLivreur(this.editedItem.id, this.editedItem)
         .then(() => {
           // Handle success
           this.showSnackbar("green", "Livreur updated successfully");
-          this.showSnackbar = true;
           this.fetchLivreurs(); // Refresh the table data
         })
         .catch((error) => {
           // Handle error
           this.showSnackbar("red", "Error updating livreur: " + error.message);
-          this.showSnackbar = true;
-        })
-        .finally(() => {
-          this.closeEditDialog();
         });
     },
-
+    saveCreate() {
+      LivreurService.addLivreur(this.editedItem)
+        .then(() => {
+          // Handle success
+          this.showSnackbar("green", "Livreur created successfully");
+          this.fetchLivreurs(); // Refresh the table data
+        })
+        .catch((error) => {
+          // Handle error
+          this.showSnackbar("red", "Error updating livreur: " + error.message);
+        });
+    },
+    editItem(item) {
+      this.editedIndex = this.livreurs.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
     openConsultDialog(item) {
       this.consultedItem = item; // Directly reference the item
       this.consultDialog = true;
+    },
+    async save() {
+      if (this.editedIndex > -1) {
+        await this.saveEdit(this.editedItem.id);
+      } else {
+        await this.saveCreate();
+      }
+      this.dialog = false;
+      this.editedItem = {
+        id: "",
+        name: "",
+        creationDate: new Date(),
+        disponibility: false,
+      };
+    },
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign(
+          {},
+          { id: "", name: "", creationDate: new Date(), disponibility: false }
+        );
+        this.editedIndex = -1;
+      });
+    },
+  },
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "New Item" : "Edit Item";
+    },
+    creationDate: {
+      get() {
+        return new Date(this.editedItem.creationDate)
+          .toISOString()
+          .split("T")[0];
+      },
+      set(creationDate) {
+        this.editedItem.creationDate = creationDate;
+      },
     },
   },
   mounted() {
--- frontend/client/src/main.js
+++ frontend/client/src/main.js
@@ -2,9 +2,21 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import '@formkit/themes/genesis'
+import { genesisIcons } from '@formkit/icons'
+import { createProPlugin, toggle } from '@formkit/pro'
+import { plugin, defaultConfig } from '@formkit/vue'
+
 
 loadFonts()
-
-createApp(App)
-  .use(vuetify)
-  .mount('#app')
+const proPlugin = createProPlugin('fk-125d8e1552e', {
+  toggle,
+})
+const app =createApp(App)
+app.use(vuetify)
+app.use(plugin, defaultConfig({theme: 'genesis' , icons: {
+    ...genesisIcons
+  },
+  plugins: [proPlugin],
+  }))
+app.mount('#app')
